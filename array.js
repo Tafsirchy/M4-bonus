55f799768c7b1b01da3134681110dbfb9f71b944
Tafsirchy <tafsirchy1000@gmail.com>
2025-08-17 11:06:24 +0600

    array

---
+++ array.js
@@ -0,0 +1,16 @@
+// const products = [
+//     { name: 'laptop', price: 50000, color: 'silver' },
+//     { name: 'phone', price: 5000, color: 'golden' },
+//     { name: 'watch', price: 3000, color: 'golden' },
+//     { name: 'camera', price: 5000, color: 'golden' },
+// ];
+
+// products[2];
+
+const products: [15, 56, 65, 78];
+const products = {
+    '0': 15,
+    '1': 56,
+    '2': 65,
+    '3': 78,
+}
